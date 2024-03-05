63cac4a54afb49237460c46980aa849b7a70c879
Kiyotakaw <kiyotakakuboki04@gmail.com>
2024-03-05 15:56:47 +0800

    register.html

---
+++ register.js
@@ -0,0 +1,24 @@
+console.log("Hello World.")
+console.error("ERROR HAPPENED!!!")
+console.warn("WARNINGGGG!!!!")
+
+// Variable
+// string = "Words" , "ahehehe" , "123"
+// number = 123, 434e, 2398.12
+// boolean = true, false
+// object = ["apple", "banana"], [1,2,3,4]
+            // ["apple", 3, true]
+
+// typeof
+// console.log(typeof(["apple", "banana"]))
+
+// Declaration of Variables
+let name = "Kiyotaka";
+ lastname = "Kuboki";
+ age = 10
+ isProf = true
+ hobbies = ["watch anime", "playing video games"]
+console.log(name, age, isProf, hobbies)
+console.table(hobbies);
+console.log("My name is " + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
